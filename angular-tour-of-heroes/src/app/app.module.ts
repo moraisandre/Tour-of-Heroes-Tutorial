e8181388d57b8d618406c3bd8d82c8c3928b7d22
André Morais <andrewinvista@gmail.com>
2017-09-08 17:34:34 -0300

    Part 4 - Services

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
-import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent],
-  imports: [
-    BrowserModule, 
-    FormsModule],
-  providers: [],
+  declarations: [AppComponent, HeroDetailComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
